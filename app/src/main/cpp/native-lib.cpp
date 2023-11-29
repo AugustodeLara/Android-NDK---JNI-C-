009e0d38a03d6a26777f13edbefe2bdcb9b29fed
aglara <augusto.lara@acad.pucrs.br>
2023-11-29 00:05:03 -0300

    basic app smartphone read csv and show events

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -13,6 +13,9 @@
 #include <regex>
 #include <iomanip>
 
+long long timeDifferenceInMillis = 0;
+long long firstEventTimeInMillis = 0;
+
 std::string removeSpaces(const std::string& str) {
     std::string result;
     for (char ch : str) {
@@ -21,6 +24,152 @@ std::string removeSpaces(const std::string& str) {
         }
     }
     return result;
+}
+
+
+bool isDateTimeInRangeToActiveTime(const std::string& dateTime, const char* startRange, const char* endRange) {
+    __android_log_print(ANDROID_LOG_ERROR, "MyApp", "Original DateTime: %s", dateTime.c_str());
+
+    // Remover espaços em branco
+    std::string dateTimeNoSpaces = removeSpaces(dateTime);
+    __android_log_print(ANDROID_LOG_ERROR, "MyApp", "Before removeSpaces: %s", dateTimeNoSpaces.c_str());
+
+    // Modificar a lógica para extrair corretamente a data/hora
+    std::istringstream dateStream(dateTimeNoSpaces);
+    int hour, minute, second;
+    char colon, period;
+
+    dateStream >> hour >> colon >> minute >> colon >> second >> period;
+
+    if (period == 'P' || period == 'p') {
+        // Se o período é PM, adicione 12 horas
+        if (hour != 12) {
+            hour += 12;
+        }
+    } else if (period == 'A' || period == 'a') {
+        // Se o período é AM e a hora é 12, defina para 0
+        if (hour == 12) {
+            hour = 0;
+        }
+    }
+
+    __android_log_print(ANDROID_LOG_ERROR, "MyApp", "After parsing: %02d:%02d:%02d", hour, minute, second);
+
+    // Adicionando a lógica para comparar com os intervalos de início e fim
+    struct tm tmDateTime = {};
+    tmDateTime.tm_hour = hour;
+    tmDateTime.tm_min = minute;
+    tmDateTime.tm_sec = second;
+
+    struct tm tmStart = {};
+    sscanf(startRange, "%d:%d:%d", &tmStart.tm_hour, &tmStart.tm_min, &tmStart.tm_sec);
+
+    struct tm tmEnd = {};
+    sscanf(endRange, "%d:%d:%d", &tmEnd.tm_hour, &tmEnd.tm_min, &tmEnd.tm_sec);
+
+    // Remova a informação de data da struct tm (definindo todos os campos para zero)
+    tmDateTime.tm_year = tmStart.tm_year = tmEnd.tm_year = 0;
+    tmDateTime.tm_mon = tmStart.tm_mon = tmEnd.tm_mon = 0;
+    tmDateTime.tm_mday = tmStart.tm_mday = tmEnd.tm_mday = 0;
+
+    // Ajuste para garantir que as comparações estejam corretas
+    if (tmDateTime.tm_hour < tmStart.tm_hour ||
+        (tmDateTime.tm_hour == tmStart.tm_hour && tmDateTime.tm_min < tmStart.tm_min) ||
+        (tmDateTime.tm_hour == tmStart.tm_hour && tmDateTime.tm_min == tmStart.tm_min && tmDateTime.tm_sec < tmStart.tm_sec)) {
+        // A data/hora está antes do início do intervalo
+        return false;
+    }
+
+    if (tmDateTime.tm_hour > tmEnd.tm_hour ||
+        (tmDateTime.tm_hour == tmEnd.tm_hour && tmDateTime.tm_min > tmEnd.tm_min) ||
+        (tmDateTime.tm_hour == tmEnd.tm_hour && tmDateTime.tm_min == tmEnd.tm_min && tmDateTime.tm_sec > tmEnd.tm_sec)) {
+        // A data/hora está após o final do intervalo
+        return false;
+    }
+
+    // O evento está dentro do intervalo
+    // Calcular a diferença em milissegundos
+    long long currentEventTimeInMillis = (hour * 3600 + minute * 60 + second) * 1000;
+    if (firstEventTimeInMillis == 0) {
+        firstEventTimeInMillis = currentEventTimeInMillis;
+    }
+
+    timeDifferenceInMillis = currentEventTimeInMillis - firstEventTimeInMillis;
+
+    return true;
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_projetomestrado_MainActivity_getTotalActiveTimeJNI(
+        JNIEnv* env,
+        jobject /* this */,
+        jstring controllerID,
+        jstring startTime,
+        jstring endTime) {
+
+    const char *controllerIDChars = env->GetStringUTFChars(controllerID, 0);
+    const char *startTimeChars = env->GetStringUTFChars(startTime, 0);
+    const char *endTimeChars = env->GetStringUTFChars(endTime, 0);
+
+    __android_log_print(ANDROID_LOG_ERROR, "getTotal", "xxx controllerID: %s", controllerIDChars);
+    __android_log_print(ANDROID_LOG_ERROR, "getTotal", "xxx startTimeChars: %s", startTimeChars);
+    __android_log_print(ANDROID_LOG_ERROR, "getTotal", "xxx endTimeChars: %s", endTimeChars);
+
+    int targetControllerID = std::atoi(controllerIDChars);
+    std::chrono::steady_clock::time_point lastEventTime;
+    std::chrono::steady_clock::duration controllerTotalTime;
+
+    const char *filePath = "/sdcard/Download/lista3.csv";
+
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        __android_log_print(ANDROID_LOG_ERROR, "getTotal", "Error opening file: %s", filePath);
+        return env->NewStringUTF("Error opening file");
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        size_t controllerPos = line.find("ID do Controlador: ");
+        if (controllerPos != std::string::npos) {
+            size_t datePos = line.find("Data/Hora: ");
+            if (datePos != std::string::npos) {
+                std::string controllerIDStr = line.substr(controllerPos + 19, line.find(",", controllerPos) - (controllerPos + 19));
+                int currentControllerID = std::stoi(controllerIDStr);
+
+                if (currentControllerID == targetControllerID) {
+                    std::string dateTime = line.substr(datePos + 11);
+                    if (isDateTimeInRangeToActiveTime(dateTime, startTimeChars, endTimeChars)) {
+                        // Se o evento está dentro do intervalo, marque o controlador como ativo
+                        __android_log_print(ANDROID_LOG_DEBUG, "getTotal", "timeDifferenceInMillis: %lld", timeDifferenceInMillis);
+                    } else {
+                        // Se o evento está fora do intervalo, marque o controlador como inativo
+                        __android_log_print(ANDROID_LOG_DEBUG, "getTotal", "Fora do intervalo",
+                                            targetControllerID, dateTime.c_str());
+                    }
+                }
+            }
+        }
+    }
+
+    file.close();
+
+    env->ReleaseStringUTFChars(controllerID, controllerIDChars);
+    env->ReleaseStringUTFChars(startTime, startTimeChars);
+    env->ReleaseStringUTFChars(endTime, endTimeChars);
+
+    // Converter a diferença de tempo para horas, minutos e segundos
+    long long hours = timeDifferenceInMillis / 3600000;
+    long long minutes = (timeDifferenceInMillis % 3600000) / 60000;
+    long long seconds = (timeDifferenceInMillis % 60000) / 1000;
+
+    // Construir uma string que contém o tempo total para o controlador específico
+    std::stringstream result;
+    result << "ID do Controlador: " << targetControllerID
+           << ", Tempo Total Ativo: " << hours << " horas, " << minutes << " minutos, " << seconds << " segundos";
+
+    __android_log_print(ANDROID_LOG_DEBUG, "getTotal", "Total Active Time for controller %d: %s", targetControllerID, result.str().c_str());
+
+    return env->NewStringUTF(result.str().c_str());
 }
 
 bool isDateTimeInRange(const std::string& dateTime, const char* startRange, const char* endRange) {
@@ -87,6 +236,8 @@ bool isDateTimeInRange(const std::string& dateTime, const char* startRange, cons
     return true;
 }
 
+
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_projetomestrado_MainActivity_readCsvFile(
         JNIEnv* env,
@@ -132,7 +283,7 @@ Java_com_example_projetomestrado_MainActivity_listEventsInRangeJNI(
     // Lógica para processar eventos no intervalo de datas
     std::stringstream eventsInRange;
 
-    const char *filePath = "/sdcard/Download/lista.csv";
+    const char *filePath = "/sdcard/Download/lista4.csv";
 
     std::ifstream file(filePath);
     if (!file.is_open()) {
--- app/src/main/java/com/example/projetomestrado/MainActivity.kt
+++ app/src/main/java/com/example/projetomestrado/MainActivity.kt
@@ -18,28 +18,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sendButton: Button
     private lateinit var readCsvButton: Button
     private lateinit var listEventsButton: Button
-
     private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1
     private var eventsInRangeResult: String = ""
-
-    // Adicionar um padrão de formatação para a classe
-    companion object {
-        val dateTimeFormat = SimpleDateFormat("h:mm:ss a", Locale.getDefault())
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textView = findViewById(R.id.sample_text)
-        sendButton = findViewById(R.id.send_button)
+        //sendButton = findViewById(R.id.send_button)
         readCsvButton = findViewById(R.id.read_csv_button)
         listEventsButton = findViewById(R.id.list_events_button)
 
-        sendButton.setOnClickListener {
-            val response = sendMessageToServer("Hello World")
-            textView.text = response
-        }
+
 
         readCsvButton.setOnClickListener {
             // Substitua "/sdcard/Download/lista.csv" pelo caminho real do seu arquivo CSV
@@ -65,6 +56,26 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        val calculateTimeButton = findViewById<Button>(R.id.calculate_time_button)
+        val resultTextView = findViewById<TextView>(R.id.result_textview)
+
+        calculateTimeButton.setOnClickListener {
+            // Obter os valores dos campos de entrada
+            val controllerIdEditText = findViewById<EditText>(R.id.controller_id_edittext)
+            val startDateEditText = findViewById<EditText>(R.id.start_date_edittext)
+            val endDateEditText = findViewById<EditText>(R.id.end_date_edittext)
+
+            val controllerId = controllerIdEditText.text.toString()
+            val startDate = startDateEditText.text.toString()
+            val endDate = endDateEditText.text.toString()
+
+            // Chamar a nova função JNI e armazenar o resultado
+            val totalActiveTimeResult = getTotalActiveTimeJNI(controllerId, startDate, endDate)
+
+            // Exibir o resultado na TextView
+            resultTextView.text = totalActiveTimeResult
+        }
+
         // Solicitar permissões em tempo de execução, se necessário
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE)
@@ -75,11 +86,13 @@ class MainActivity : AppCompatActivity() {
     external fun readCsvFile(filePath: String): String
 
     // Funções JNI existentes (Hello World e SendMessageToServer)
-    external fun stringFromJNI(): String
     external fun sendMessageToServer(message: String): String
 
     // Função JNI para listar eventos no intervalo de datas
     external fun listEventsInRangeJNI(startDate: String, endDate: String): String
+
+    external fun getTotalActiveTimeJNI(controllerID: String, startTime: String, endTime: String): String
+
 
     init {
         System.loadLibrary("projetomestrado")
